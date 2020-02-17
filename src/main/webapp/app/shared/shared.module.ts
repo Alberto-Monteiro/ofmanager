b9ac9366b9d89a1921a36ebb673239c5719e6238
Alberto Monteiro <alberto.costa.monteiro@gmail.com>
2020-02-17 18:05:33 -0300

    Incluindo o angular material

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { OfmanagerSharedLibsModule } from './shared-libs.module';
-import { FindLanguageFromKeyPipe } from './language/find-language-from-key.pipe';
-import { AlertComponent } from './alert/alert.component';
-import { AlertErrorComponent } from './alert/alert-error.component';
-import { LoginModalComponent } from './login/login.component';
-import { HasAnyAuthorityDirective } from './auth/has-any-authority.directive';
-import { ServicoOfDeleteDialogComponent } from 'app/entities/servico-of/servico-of-delete-dialog.component';
-import { IConfig, NgxMaskModule } from 'ngx-mask';
-import { ToastrModule } from 'ngx-toastr';
+import {NgModule} from '@angular/core';
+import {OfmanagerSharedLibsModule} from './shared-libs.module';
+import {FindLanguageFromKeyPipe} from './language/find-language-from-key.pipe';
+import {AlertComponent} from './alert/alert.component';
+import {AlertErrorComponent} from './alert/alert-error.component';
+import {LoginModalComponent} from './login/login.component';
+import {HasAnyAuthorityDirective} from './auth/has-any-authority.directive';
+import {ServicoOfDeleteDialogComponent} from 'app/entities/servico-of/servico-of-delete-dialog.component';
+import {IConfig, NgxMaskModule} from 'ngx-mask';
+import {ToastrModule} from 'ngx-toastr';
+import {MaterialModule} from 'app/shared/material-module';
 
 export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
 
@@ -28,7 +29,9 @@ export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
     AlertComponent,
     AlertErrorComponent,
     LoginModalComponent,
-    HasAnyAuthorityDirective
+    HasAnyAuthorityDirective,
+    MaterialModule
   ]
 })
-export class OfmanagerSharedModule {}
+export class OfmanagerSharedModule {
+}
